ae62e2cf5bd8a8862c78c67a4315bb399612b44b
Rakshith N <rakshith.n@atos.net>
2022-08-02 12:26:00 +0530

    updated requirements.txt

--- chartink_ops.py
+++ chartink_ops.py
@@ -1,12 +1,9 @@
 from config import Config as conf
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
 import os
-import re
 import pandas as pd
-import stat
 
 class ChartInkOps:
     def __init__(self) -> None:
